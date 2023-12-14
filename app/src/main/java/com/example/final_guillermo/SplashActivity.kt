0e62011f9764d317c05a8718f287e70d623646f8
alvarolpzq43 <lopezquira10gmail.com>
2023-12-14 16:00:26 -0500

    a

--- app/src/main/java/com/example/final_guillermo/SplashActivity.kt
+++ app/src/main/java/com/example/final_guillermo/SplashActivity.kt
@@ -1,12 +1,26 @@
 package com.example.final_guillermo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import com.example.final_guillermo.databinding.ActivitySplashBinding
 
-class SplashActivity : AppCompatActivity() {
+class Splash : AppCompatActivity() {
 
+    private lateinit var binding: ActivitySplashBinding
+    private val SPLASH_TIME_OUT: Long = 1000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+        Handler().postDelayed({
+            val intent = Intent(this,login::class.java)
+            startActivity(intent)
+
+            finish()
+        },SPLASH_TIME_OUT)
     }
 }

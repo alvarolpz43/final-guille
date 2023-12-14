551e804adefd65c2f8fbdf1d2c77e0dc85b5b9d9
wodman43 <lopezquira10gmail.com>
2023-12-14 00:04:30 -0500

    ya acabe las rutas gg

--- app/src/main/java/com/example/final_guillermo/ApiService.kt
+++ app/src/main/java/com/example/final_guillermo/ApiService.kt
@@ -1,15 +1,14 @@
 package com.example.final_guillermo
 
-import kotlinx.coroutines.CoroutineStart
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
     @GET("/v2/directions/driving-car")
-    fun getRoute(
+    suspend fun getRoute(
         @Query("api_key") key: String,
-        @Query("start") start: String,
-        @Query("end") end: String
-    ):Response<*>
+        @Query("start", encoded = true) start: String,
+        @Query("end", encoded = true) end: String
+    ):Response<RouteResponse>
 }
--- app/src/main/java/com/example/final_guillermo/RouteMapActivity.kt
+++ app/src/main/java/com/example/final_guillermo/RouteMapActivity.kt
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.maps.model.PolylineOptions
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
@@ -21,6 +24,7 @@ class RouteMapActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var btnCalculate: Button
     private var start: String = ""
     private var end: String = ""
+    var poly: Polyline? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +35,9 @@ class RouteMapActivity : AppCompatActivity(), OnMapReadyCallback {
         btnCalculate.setOnClickListener {
             start = ""
             end = ""
-
+            poly?.remove()
+            poly= null
+            Toast.makeText(this,"Selecciona punto de origen y final",Toast.LENGTH_SHORT).show()
             if (::map.isInitialized) {
                 map.setOnMapClickListener {
                     if (start.isEmpty()) {
@@ -52,19 +58,29 @@ class RouteMapActivity : AppCompatActivity(), OnMapReadyCallback {
         this.map = map
     }
 
-    fun createRoute() {
+    private fun createRoute() {
         CoroutineScope(Dispatchers.IO).launch {
             val call = getRetrofit().create(ApiService::class.java)
-                .getRoute("5b3ce3597851110001cf6248c50b947f1222418498fae123bb1a6114", start,end)
-            if (call.isSuccessful){
-                Log.i("aris","OK")
-            }else{
-                Log.i("aris","OK")
+                .getRoute("5b3ce3597851110001cf6248c50b947f1222418498fae123bb1a6114", start, end)
+            if (call.isSuccessful) {
+                drawRoute(call.body())
+            } else {
+                Log.i("aris", "OK")
             }
         }
     }
 
-    fun getRetrofit(): Retrofit {
+    private fun drawRoute(routeResponse: RouteResponse?) {
+        val polyLineOptions = PolylineOptions()
+        routeResponse?.features?.first()?.geometry?.coordinates?.forEach {
+            polyLineOptions.add(LatLng(it[1], it[0]))
+        }
+        runOnUiThread {
+          poly =  map.addPolyline(polyLineOptions)
+        }
+    }
+
+    private fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://api.openrouteservice.org/")
             .addConverterFactory(GsonConverterFactory.create())
